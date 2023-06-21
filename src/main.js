6b03919e336cdcd48d317ed19836ede3610d4ad4
manuel_859 <manuel_859@outlook.es>
2023-06-21 17:03:46 -0600

    terminación de codigo js

--- src/main.js
+++ src/main.js
@@ -1,48 +1,71 @@
-// Datos de ejemplo
-const productos = [
-    { nombre: 'Producto 1', categoria: 'Electrónica', precio: 100 },
-    { nombre: 'Producto 2', categoria: 'Ropa', precio: 50 },
-    { nombre: 'Producto 3', categoria: 'Hogar', precio: 200 },
-    // Agrega más productos aquí
-  ];
-  
-  // Obtener referencias a los elementos HTML
-  const categorySelect = document.getElementById('category');
-  const priceInput = document.getElementById('price');
-  const productList = document.getElementById('productList');
-  const filterBtn = document.getElementById('filterBtn');
-  
-  // Función para mostrar los productos filtrados
-  function mostrarProductosFiltrados() {
-    const categoriaSeleccionada = categorySelect.value;
-    const precioSeleccionado = parseFloat(priceInput.value);
-  
-    // Filtrar productos según las selecciones del usuario
-    const productosFiltrados = productos.filter(producto => {
-      if (categoriaSeleccionada && producto.categoria !== categoriaSeleccionada) {
-        return false;
-      }
-      if (precioSeleccionado && producto.precio > precioSeleccionado) {
-        return false;
-      }
-      return true;
-    });
-  
+    // URL de la API de productos
+    const apiUrl = 'https://rickandmortyapi.com/api/character/?page=19';
+
+    // Obtener referencias a los elementos HTML
+    const productList = document.getElementById('productList');
+
+    // Función para cargar los productos desde la API
+    async function cargarProductos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.results; // Obtener los resultados de la API
+    } catch (error) {
+        console.error('Error al cargar los productos:', error);
+        return [];
+    }
+    }
+
+
+
+    // Función para mostrar los productos filtrados
+    async function mostrarProductosFiltrados() {
+    // Cargar productos desde la API
+    const productos = await cargarProductos();
+
     // Limpiar lista de productos
     productList.innerHTML = '';
-  
+
     // Mostrar productos filtrados
-    productosFiltrados.forEach(producto => {
-      const productElement = document.createElement('div');
-      productElement.classList.add('product');
-      productElement.innerText = `${producto.nombre}\nCategoría: ${producto.categoria}\nPrecio: $${producto.precio}`;
-      productList.appendChild(productElement);
+    productos.forEach(personaje => {
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        cardElement.style.width = '19rem';
+        cardElement.style.marginBottom = '1.25rem';
+        cardElement.style.marginLeft = '2.55rem';
+        
+
+
+        const imageElement = document.createElement('img');
+        imageElement.classList.add('card-img-top');
+        imageElement.src = personaje.image; // Utilizar la propiedad 'image' del objeto 'personaje'
+        imageElement.alt = personaje.name;
+        cardElement.appendChild(imageElement);
+
+        const cardBodyElement = document.createElement('div');
+        cardBodyElement.classList.add('card-body');
+        cardElement.appendChild(cardBodyElement);
+
+        const titleElement = document.createElement('h5');
+        titleElement.classList.add('card-title');
+        titleElement.textContent = personaje.name;
+        cardBodyElement.appendChild(titleElement);
+        cardBodyElement.style.textAlign = 'center';
+
+        const descriptionElement = document.createElement('p');
+        descriptionElement.classList.add('card-text');
+        descriptionElement.textContent = `Status: ${personaje.status}, Species: ${personaje.species}`; // Agregar más información del personaje si deseas
+        cardBodyElement.appendChild(descriptionElement);
+
+        const linkElement = document.createElement('a');
+        linkElement.classList.add('btn', 'btn-primary');
+        linkElement.href = '#';
+        linkElement.textContent = 'ver este episodio';
+        cardBodyElement.appendChild(linkElement);
+
+        productList.appendChild(cardElement);
     });
-  }
-  
-  // Asignar evento click al botón de filtrar
-  filterBtn.addEventListener('click', mostrarProductosFiltrados);
-  
-  // Mostrar todos los productos al cargar la página
-  mostrarProductosFiltrados();
-  
+    }
+
+    // Mostrar todos los productos al cargar la página
+    mostrarProductosFiltrados();
